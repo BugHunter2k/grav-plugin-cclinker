c113b6e08c37ad831373deaa028519d40c3bdb1e
Ingo Hollmann <bughunter2k@gmail.com>
2017-01-11 21:22:52 +0100

    Fixe for #3
    If grav is installed in a subfoler we need to get that in the path too.

--- cclinker.php
+++ cclinker.php
@@ -65,8 +65,9 @@ class CclinkerPlugin extends Plugin
 			$v = $type[2];
 			if (strlen($v) == 1) $v .= ".0";
 		}
-                $locator = GravTrait::getGrav()['locator'];
-                $path = "/".$locator->findResource('plugin://cclinker/images/', false)."/";
+		$grav = GravTrait::getGrav();
+                $locator = $grav['locator'];
+                $path = $grav['base_url']."/".$locator->findResource('plugin://cclinker/images/', false)."/";
                 switch($this->config->get('plugins.cclinker.outputformat')) {
                     case "text": 
                             $linktext = $this->licenses[$l][1];
